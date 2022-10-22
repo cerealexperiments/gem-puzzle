d13ab012bb374f7bdd82100c295bc136a729f251
rin <prettysunshine468@gmail.com>
2022-10-22 21:22:53 +0600

    implement functionality to change the grid size; major refactoring

--- script.js
+++ script.js
@@ -1,35 +1,117 @@
-const gridItems = document.querySelectorAll(".grid__item");
-const buttonRestart = document.querySelector(".button--restart");
-const movesCount = document.querySelector(".moves__count");
-const timerElement = document.querySelector(".time__timer");
-
+// game config
 let seconds = 0;
 let minutes = 0;
 let timer;
 let arraySize = 4;
-startTimer();
-
 let moves = 0;
-movesCount.textContent = moves;
 
-buttonRestart.addEventListener("click", () => {
+const gridItems = document.getElementsByClassName("grid__item");
+
+// generate html
+const containerElement = document.createElement("div");
+containerElement.classList.add("container");
+document.body.prepend(containerElement);
+
+const buttonsElement = document.createElement("div");
+buttonsElement.classList.add("buttons");
+containerElement.append(buttonsElement);
+
+const buttonRestartElement = document.createElement("button");
+buttonRestartElement.classList.add("button", "button--restart");
+buttonRestartElement.textContent = "Shuffle and restart";
+buttonsElement.append(buttonRestartElement);
+
+const buttonSaveElement = document.createElement("button");
+buttonSaveElement.classList.add("button", "button--save");
+buttonSaveElement.textContent = "Save";
+buttonsElement.append(buttonSaveElement);
+
+const buttonResultsElement = document.createElement("button");
+buttonResultsElement.classList.add("button", "button--results");
+buttonResultsElement.textContent = "Results";
+buttonsElement.append(buttonResultsElement);
+
+const statsElement = document.createElement("div");
+statsElement.classList.add("stats");
+containerElement.append(statsElement);
+
+const movesElement = document.createElement("p");
+movesElement.classList.add("moves");
+movesElement.innerHTML = "Moves: ";
+statsElement.append(movesElement);
+
+const timeElement = document.createElement("p");
+timeElement.classList.add("time");
+timeElement.textContent = "Time: ";
+statsElement.append(timeElement);
+
+const timerElement = document.createElement("span");
+timerElement.classList.add("time__timer");
+timerElement.textContent = "00:00";
+timeElement.append(timerElement);
+
+const movesCountElement = document.createElement("span");
+movesCountElement.classList.add("moves__count");
+movesCountElement.innerHTML = "0";
+movesElement.append(movesCountElement);
+
+let gridElement = document.createElement("div");
+let gameGrid = [];
+containerElement.append(gridElement);
+
+const frameSizeElement = document.createElement("p");
+frameSizeElement.classList.add("frame-size");
+frameSizeElement.textContent = `Frame size: ${arraySize}x${arraySize}`;
+containerElement.append(frameSizeElement);
+
+const otherSizesElement = document.createElement("div");
+otherSizesElement.classList.add("other-sizes");
+otherSizesElement.textContent = "Other sizes: ";
+containerElement.append(otherSizesElement);
+
+for (let i = 3; i < 9; i++) {
+  let otherSize = document.createElement("p");
+  otherSize.classList.add("other-size");
+  otherSize.textContent = `${i}x${i}`;
+  otherSize.addEventListener("click", () => {
+    arraySize = i;
+    startGame(arraySize);
+  });
+  otherSizesElement.append(otherSize);
+}
+
+buttonRestartElement.addEventListener("click", () => {
   resetGame();
 });
 
-let gameGrid = generateArray(arraySize);
-activateGrid(arraySize);
+startGame(arraySize);
 
-// starts the game
-for (let i = 0; i < gridItems.length; i++) {
-  gridItems[i].textContent = gameGrid[i];
-  if (gridItems[i].textContent === "") {
-    gridItems[i].classList.add("empty");
-  } else {
-    gridItems[i].classList.remove("empty");
-  }
+function startGame(gridSize) {
+  pauseTimer();
+  resetGame();
+  initGameGrid(gridSize);
+  activateGrid(gridSize);
+  startTimer();
+  frameSizeElement.textContent = `Frame size: ${arraySize}x${arraySize}`;
 }
 
-console.log(gameGrid);
+function initGameGrid(gridSize) {
+  gridElement.innerHTML = null;
+  gameGrid = generateArray(gridSize);
+  gridElement.className = "";
+  gridElement.classList.add(`grid--${gridSize}x${gridSize}`);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let gridItem = document.createElement("div");
+    gridItem.classList.add("grid__item");
+    gridItem.textContent = gameGrid[i];
+    if (gridItem.textContent === "") {
+      gridItem.classList.add("empty");
+    } else {
+      gridItem.classList.remove("empty");
+    }
+    gridElement.append(gridItem);
+  }
+}
 
 function activateGrid(arraySize) {
   for (let i = 0; i < gridItems.length; i++) {
@@ -38,43 +120,32 @@ function activateGrid(arraySize) {
         console.log("empty cell clicked");
       } else {
         if (gridItems[i + 1] && gridItems[i + 1].textContent === "") {
-          gridItems[i + 1].classList.toggle("empty");
-          gridItems[i + 1].textContent = gridItems[i].textContent;
-          gridItems[i].classList.toggle("empty");
-          gridItems[i].textContent = "";
-          moves++;
-          movesCount.textContent = moves;
+          moveCell(i, i + 1);
         } else if (gridItems[i - 1] && gridItems[i - 1].textContent === "") {
-          gridItems[i - 1].classList.toggle("empty");
-          gridItems[i - 1].textContent = gridItems[i].textContent;
-          gridItems[i].classList.toggle("empty");
-          gridItems[i].textContent = "";
-          moves++;
-          movesCount.textContent = moves;
+          moveCell(i, i - 1);
         } else if (
           gridItems[i + arraySize] &&
           gridItems[i + arraySize].textContent === ""
         ) {
-          gridItems[i + arraySize].classList.toggle("empty");
-          gridItems[i + arraySize].textContent = gridItems[i].textContent;
-          gridItems[i].classList.toggle("empty");
-          gridItems[i].textContent = "";
-          moves++;
-          movesCount.textContent = moves;
+          moveCell(i, i + arraySize);
         } else if (
           gridItems[i - arraySize] &&
           gridItems[i - arraySize].textContent === ""
         ) {
-          gridItems[i - arraySize].classList.toggle("empty");
-          gridItems[i - arraySize].textContent = gridItems[i].textContent;
-          gridItems[i].classList.toggle("empty");
-          gridItems[i].textContent = "";
-          moves++;
-          movesCount.textContent = moves;
+          moveCell(i, i - arraySize);
         }
       }
     });
   }
+}
+
+function moveCell(cellIndex, emptyCellIndex) {
+  gridItems[emptyCellIndex].classList.toggle("empty");
+  gridItems[emptyCellIndex].textContent = gridItems[cellIndex].textContent;
+  gridItems[cellIndex].classList.toggle("empty");
+  gridItems[cellIndex].textContent = "";
+  moves++;
+  movesCountElement.textContent = moves;
 }
 
 function resetGame() {
@@ -82,17 +153,9 @@ function resetGame() {
   seconds = 0;
   minutes = 0;
   moves = 0;
-  movesCount.textContent = moves;
-  gameGrid = generateArray(4);
-  gameGrid = shuffleArray(gameGrid);
-  for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].textContent = gameGrid[i];
-    if (gridItems[i].textContent === "") {
-      gridItems[i].classList.add("empty");
-    } else {
-      gridItems[i].classList.remove("empty");
-    }
-  }
+  movesCountElement.textContent = moves;
+  initGameGrid(arraySize);
+  activateGrid(arraySize);
   console.log(gameGrid);
 }
 
@@ -116,7 +179,7 @@ function pauseTimer() {
 
 function generateArray(size) {
   let array = [];
-  for (let i = 0; i < size * size; i++) {
+  for (let i = 1; i <= size * size; i++) {
     array.push(i);
   }
   const emptyIndex = Math.floor(Math.random() * (array.length - 1));
